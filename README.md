4abbe2f440dc6ac35274849191e009324d643c3b
singcl <24661881@qq.com>
2018-06-14 17:45:23 +0800

    :art: 变量赋值 和 常量定义

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### 编译输出到指定目录
 
 $ go build -o ./exe/values ./values.go
+
+### 实例来源
+https://gobyexample.com/
---
+++ constants.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+import "math"
+
+const s string = "constant"
+
+func main() {
+	fmt.Println(s)
+
+	const n = 500000000
+	const d = 3e+20 / n
+
+	fmt.Println(d)
+	fmt.Println(int64(d))
+	fmt.Println(math.Sin(n))
+}
