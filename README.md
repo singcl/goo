5f986842e6a66f23f68a0fff52f89e5d44103a9d
singcl <24661881@qq.com>
2018-06-14 17:13:58 +0800

    :art: 简单的编译

---
+++ README.md
@@ -0,0 +1,3 @@
+### 编译输出到指定目录
+
+$ go build -o ./exe/values ./values.go
--- hello.go
+++ hello.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Println("Hello, World!")
-}
+// func main() {
+// 	fmt.Println("Hello, World!")
+// }
