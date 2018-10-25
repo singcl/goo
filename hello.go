a9ceae7ebe9f2b3bef683a2396de392bdb69f852
singcl <24661881@qq.com>
2018-10-25 14:44:11 +0800

    :art:hello

--- hello.go
+++ hello.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	var ch int = '\u0041'
+	var ch2 int = '\u03B2'
+	var ch3 int = '\U00101234'
+	fmt.Printf("%d - %d - %d\n", ch, ch2, ch3) // integer
+	fmt.Printf("%c - %c - %c\n", ch, ch2, ch3) // character
+	fmt.Printf("%X - %X - %X\n", ch, ch2, ch3) // UTF-8 bytes
+	fmt.Printf("%U - %U - %U", ch, ch2, ch3) // UTF-8 code point
 }
