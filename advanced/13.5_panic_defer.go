0d662a06d00ee1fb9dce739ca9e28c8615431bac
singcl <24661881@qq.com>
2018-10-31 16:34:52 +0800

    :art: 一种用闭包处理错误的模式

---
+++ advanced/13.5_panic_defer.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	ff()
+	fmt.Println("Returned normally from f.")
+}
+
+func ff() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
+}
