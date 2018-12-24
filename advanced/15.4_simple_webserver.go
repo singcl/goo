7635369aeb09ab58fc0c0da07e793259d805c4ba
singcl <1276837689@qq.com>
2018-12-24 19:04:51 +0800

    :art: 函数类型的类型别名 和 强制类型转换

--- advanced/15.4_simple_webserver.go
+++ advanced/15.4_simple_webserver.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"net/http"
-	"io"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 const form = `
@@ -39,9 +39,20 @@ func Home(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "<h1>Home %s </h1>", req.Method)
 }
 
+// type HandlerFunc func(ResponseWriter, *Request)
+// 这里的类型别名和其他语言的类型别名不一样： HandlerFunc 和 func(ResponseWriter, *Request) 是两个不同的类型
+// 要想兼容必须显示强制类型转换
+
 func main() {
 	http.HandleFunc("/test1", SimpleServer)
 	http.HandleFunc("/test2", FormServer)
+	// type Handler interface {
+	// 	ServeHTTP(ResponseWriter, *Request)
+	// }
+	// type HandlerFunc func(ResponseWriter, *Request)
+	// http.HandlerFunc 是定义在server.go中的一个类型
+	// 这里http.HandlerFunc(Home)不是执行函数，而是强制类型转换,
+	// http.HandlerFunc类型又实现了Handler接口，所有又是Handler接口类型
 	http.Handle("/", http.HandlerFunc(Home))
 
 	if err := http.ListenAndServe(":50000", nil); err != nil {
